621d35a1f678b58a15a07e468689f66e6b4da2cd
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-06-23 22:01:48 +0800

    dynamic arrow degree

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-
 import TypingText from "@/components/TypingText.vue";
+import type { Ref } from "vue";
+import { ref, watch } from "vue";
+
+const main: Ref<HTMLElement | null> = ref(null);
+const degree = ref(0);
+
+function scroll() {
+  const pos = main.value?.scrollTop || 0;
+  degree.value = (pos / document.body.clientHeight) * 360;
+}
 </script>
 
 <template>
-  <main>
+  <main ref="main" @scroll="scroll">
     <div class="fabric">
       <img src="/background.webp" class="background" alt />
       <div class="avatar">
@@ -12,7 +21,7 @@ import TypingText from "@/components/TypingText.vue";
       </div>
       <TypingText content="Nothing is impossible." class="quote" />
       <div class="arrow">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" :style="{'transform': `rotate(${degree}deg)`}">
           <path d="m12 15.586-4.293-4.293-1.414 1.414L12 18.414l5.707-5.707-1.414-1.414z"></path>
           <path d="m17.707 7.707-1.414-1.414L12 10.586 7.707 6.293 6.293 7.707 12 13.414z"></path>
         </svg>
@@ -90,18 +99,25 @@ main {
   top: calc(-20vh - 56px);
   left: 50%;
   transform: translate(-50%, -50%);
-  width: max-content;
+  width: 36px;
+  height: 36px;
+  padding: 2px;
+  background: rgba(255,255,255,.1);
+  backdrop-filter: blur(2px);
+  border-radius: 50%;
 }
 
 .fabric .arrow svg {
   fill: #fff;
   width: 32px;
   height: 32px;
+  transition: transform .2s ease-in-out;
 }
 
 .container {
   position: relative;
   transform: translateY(100%);
+  background: #1E1E1EFF;
   width: 100%;
   height: 100%;
   display: flex;
