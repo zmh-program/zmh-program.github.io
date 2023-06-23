534962b13e11be6ed60eb96a631728dc915289e8
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-06-23 18:43:02 +0800

    rewrite

--- src/components/animation-delay.vue
+++
@@ -1,6 +0,0 @@
-function set_animation_delay(cls, delay_s, bias) {
-    const elements = document.getElementsByClassName(cls);
-    for (let i = 0,  length = elements.length; i < length; i++) {
-        elements[i].style.animationDuration = (delay_s * i + bias) + "s";
-    }
-}
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <img class="background" src="/background.webp" alt />
+  </main>
+</template>
+
+<style scoped>
+.background {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+}
+</style>
