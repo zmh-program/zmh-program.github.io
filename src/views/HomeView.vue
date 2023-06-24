897ae8fb2e8d5fa91109615071ca3cbf5467f6d2
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-06-24 10:08:10 +0800

    update icons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,10 @@ import TypingText from "@/components/TypingText.vue";
 import { scrollTo } from "@/assets/script/scroll";
 import type { Ref } from "vue";
 import { ref } from "vue";
+import Github from "@/components/icons/github.vue";
+import Twitter from "@/components/icons/twitter.vue";
+import Blog from "@/components/icons/blog.vue";
+import Arrow from "@/components/icons/arrow.vue";
 
 const main: Ref<HTMLElement | null> = ref(null);
 const degree = ref(0);
@@ -31,13 +35,7 @@ function scroll() {
       <div class="avatar"><img src="/avatar/zmh-program.png" alt="zmh-program" /></div>
       <TypingText content="Nothing is impossible." class="quote" />
     </div>
-    <div class="arrow" @click="scroll">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-           :style="{'transform': `rotate(${degree}deg)`}">
-        <path d="m12 15.586-4.293-4.293-1.414 1.414L12 18.414l5.707-5.707-1.414-1.414z"></path>
-        <path d="m17.707 7.707-1.414-1.414L12 10.586 7.707 6.293 6.293 7.707 12 13.414z"></path>
-      </svg>
-    </div>
+    <div class="arrow" @click="scroll"><arrow :degree="degree" /></div>
     <div class="circle" />
     <div class="container">
       <div class="column about">
@@ -49,15 +47,9 @@ function scroll() {
         <p>前方的道路还很长，请多指教！</p>
         <a href="https://github.com/zmh-program" class="stats"><img src="https://stats.deeptrain.net/user/zmh-program" alt="zmh-program's Github Stats"></a>
         <div class="icons">
-          <a href="https://github.com/zmh-program" target="_blank">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336 9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"></path></svg>
-          </a>
-          <a href="https://twitter.com/ProgramZmh" target="_blank">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809.324.037.636.05.973.05a8.07 8.07 0 0 0 5.001-1.721 4.036 4.036 0 0 1-3.767-2.793c.249.037.499.062.761.062.361 0 .724-.05 1.061-.137a4.027 4.027 0 0 1-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 0 1-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 0 0 8.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 0 1 4.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 0 0 2.556-.973 4.02 4.02 0 0 1-1.771 2.22 8.073 8.073 0 0 0 2.319-.624 8.645 8.645 0 0 1-2.019 2.083z"></path></svg>
-          </a>
-          <a href="https://zmh-program.blog.csdn.net" target="_blank">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 3H4c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2zm-1 4v2h-5V7h5zm-5 4h5v2h-5v-2zM4 19V5h7v14H4z"></path></svg>
-          </a>
+          <a href="https://github.com/zmh-program" target="_blank"><github /></a>
+          <a href="https://twitter.com/ProgramZmh" target="_blank"><twitter /></a>
+          <a href="https://zmh-program.blog.csdn.net" target="_blank"><blog /></a>
         </div>
       </div>
 
@@ -150,7 +142,7 @@ function scroll() {
             <p>
               小学生，在学高数（<br><br>
               “我偏向竞赛的，最多就拿了个 <span>CSP-J</span> 广西一等奖<br>
-              我会一点easyx，和ue5,unity的游戏制作（真的一点点）”<br>
+              我会一点 <span>easyx</span> 和 <span>ue5, unity</span> 的游戏制作（真的一点点）”<br>
               ——来自航小怕
             </p>
           </div>
