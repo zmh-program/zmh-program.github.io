67a0f37a80b9d38d3d0c8ed8a10939b8ac413f93
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-06-24 08:05:12 +0800

    update project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -40,17 +40,40 @@ function scroll() {
     </div>
     <div class="circle" />
     <div class="container">
-      <div class="column">
+      <div class="column about">
         <h2>关于</h2>
         <p>我是 <span>zmh-program</span>，生于 <span>2009</span> 年。</p>
         <p>热爱深度强化学习、喜爱钻研网络相关的知识，</p>
         <p>对前后端有一定的理解，涉猎大数据、爬虫、网络攻防、云原生和云计算等领域。</p>
         <p>喜欢哪个就学哪个！热衷探索，喜爱信息技术的一切！</p>
         <p>前方的道路还很长，请多指教！</p>
+        <div class="icons">
+          <a href="https://github.com/zmh-program" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336 9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"></path></svg>
+          </a>
+          <a href="https://twitter.com/ProgramZmh" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809.324.037.636.05.973.05a8.07 8.07 0 0 0 5.001-1.721 4.036 4.036 0 0 1-3.767-2.793c.249.037.499.062.761.062.361 0 .724-.05 1.061-.137a4.027 4.027 0 0 1-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 0 1-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 0 0 8.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 0 1 4.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 0 0 2.556-.973 4.02 4.02 0 0 1-1.771 2.22 8.073 8.073 0 0 0 2.319-.624 8.645 8.645 0 0 1-2.019 2.083z"></path></svg>
+          </a>
+          <a href="https://zmh-program.blog.csdn.net" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 3H4c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2zm-1 4v2h-5V7h5zm-5 4h5v2h-5v-2zM4 19V5h7v14H4z"></path></svg>
+          </a>
+        </div>
       </div>
 
       <div class="column project">
         <h2>我的作品</h2>
+        <div class="works">
+          <a class="work" href="https://deeptrain.net">
+            <img src="/project/deeptrain.png" alt="">
+            <h3>Deeptrain</h3>
+            <p>🔥 Deeptrain, 一个开源深度强化学习训练平台</p>
+          </a>
+          <a class="work" href="https://stats.deeptrain.net">
+            <img src="/project/stats.png" alt="">
+            <h3>Code Statistic</h3>
+            <p>⚡ 动态生成你的 GitHub 统计卡片！</p>
+          </a>
+        </div>
       </div>
 
       <a class="icp" href="https://beian.miit.gov.cn/" target="_blank">
@@ -277,5 +300,65 @@ main {
 
 .icp:hover img {
   opacity: 1;
+}
+
+.icons {
+  width: max-content;
+  margin: 10px auto 0;
+  transform: translateY(10px);
+}
+
+.icons a {
+  background: none !important;
+  margin: 6px;
+}
+
+.icons svg {
+  width: 32px;
+  height: 32px;
+  fill: #ccc;
+  transition: .25s;
+}
+
+.icons svg:hover {
+  fill: #fff;
+}
+
+.works {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  max-width: min(1080px, 100%);
+  width: max-content;
+  margin: 0 auto;
+  gap: 32px;
+}
+
+.works .work {
+  width: 300px;
+  height: 200px;
+  background: rgb(30,30,30);
+  display: flex;
+  flex-direction: column;
+  box-shadow: rgba(0,0,0,.03) 0 5px 10px;
+  text-align: center;
+  align-items: center;
+  overflow: hidden;
+  transition: .25s;
+  cursor: pointer;
+  border-radius: 8px;
+  padding: 20px 10px;
+}
+
+.work img {
+  height: 60px;
+  width: 60px;
+}
+.work h3 {
+  color: #fff;
+}
+.work p {
+  margin-top: 6px;
+  max-width: 200px;
 }
 </style>
