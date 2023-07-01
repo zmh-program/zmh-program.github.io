c99644d7ff1595e5462701508d318c8bafe1790d
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-07-01 10:54:56 +0800

    add html minify and service worker

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 ### 🍉 Project Description
 This is a portfolio website that showcases my work and skills. It is built with Vue and Typescript. It is a responsive website that can be viewed on any device.
+
+
+### Features
+- [x] ✨ SEO Optimized
+- [x] ⚡ Service Worker
+- [x] 🍎 Responsive
+- [x] 🔮 Dark Mode
+- [x] 🍓 Animations
+- [x] 🍉 Information Card
+- [x] 🎃 Magical Arrow
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {VitePWA} from "vite-plugin-pwa";
+import {createHtmlPlugin} from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA(),
+    createHtmlPlugin({
+      minify: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
