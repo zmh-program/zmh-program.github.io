61711414601ace1e76657de890cbbf3358b52d61
Zhang Minghan <zmh13054618081@dingtalk.com>
2024-09-15 05:21:59 +0800

    chore: fix mobile adapter

--- src/components/magicui/meteors.tsx
+++ src/components/magicui/meteors.tsx
@@ -13,23 +13,32 @@ export const Meteors = ({ number = 20 }: MeteorsProps) => {
   );
 
   useEffect(() => {
-    const styles = [...new Array(number)].map(() => ({
-      top: -5,
-      left: Math.floor(Math.random() * window.innerWidth) + "px",
-      animationDelay: Math.random() * 1 + 0.2 + "s",
-      animationDuration: Math.floor(Math.random() * 8 + 2) + "s",
-    }));
-    setMeteorStyles(styles);
+    const updateMeteorStyles = () => {
+      const styles = [...new Array(number)].map(() => ({
+        top: -5,
+        left: Math.floor(Math.random() * Math.min(window.innerWidth, document.documentElement.clientWidth)) + "px",
+        animationDelay: Math.random() * 1 + 0.2 + "s",
+        animationDuration: Math.floor(Math.random() * 8 + 2) + "s",
+      }));
+      setMeteorStyles(styles);
+    };
+
+    updateMeteorStyles();
+    window.addEventListener('resize', updateMeteorStyles);
+
+    return () => {
+      window.removeEventListener('resize', updateMeteorStyles);
+    };
   }, [number]);
 
   return (
     <>
-      {[...meteorStyles].map((style, idx) => (
+      {meteorStyles.map((style, idx) => (
         // Meteor Head
         <span
           key={idx}
           className={cn(
-            "-z-10 pointer-events-none absolute left-1/2 top-1/2 size-0.5 rotate-[215deg] animate-meteor rounded-full bg-black/60 shadow-[0_0_0_1px_#ffffff10]",
+            "-z-10 pointer-events-none fixed left-1/2 top-1/2 size-0.5 rotate-[215deg] animate-meteor rounded-full bg-black/60 shadow-[0_0_0_1px_#ffffff10]",
           )}
           style={style}
         >
