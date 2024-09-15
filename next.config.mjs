b2ab695310b60616db255a96cb012ffeef744381
Zhang Minghan <zmh13054618081@dingtalk.com>
2024-09-15 20:27:54 +0800

    feat: update pwa cache conf

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,31 @@ import PWABuilder from 'next-pwa';
 
 const withPWA = PWABuilder({
   dest: 'public',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /\.(css|js)$/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'static-assets',
+      },
+    },
+    {
+      urlPattern: /\.(png|jpg|jpeg|svg|gif|webp)$/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images',
+      },
+    },
+    {
+      urlPattern: /\.(woff|woff2|eot|ttf|otf)$/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'fonts',
+      },
+    },
+  ],
 });
 
 const nextConfig = withPWA({
